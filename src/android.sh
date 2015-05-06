1ed0e752ca13ea667828628b213fd860a921cea6
YourtionGuo <yourtion@gmail.com>
2015-05-06 15:05:44 +0800

    update icon configure

--- src/android.sh
+++ src/android.sh
@@ -1,8 +1,8 @@
-sizes=(144 96 72 29)
+sizes=(144 96 72 48)
 OLDIFS="$IFS"
 IFS=$'\n'
 while read f ; do
-	for size in ${sizes[@]};do	
+	for size in ${sizes[@]};do
 		sips -Z $size $f -o ${f/.png/_$size.png}
 	done
 done
